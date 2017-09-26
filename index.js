d9d6421751ba7e3c8d54aff2e7b8623b466f4fbb
Jose Riano <joselriano10@gmail.com>
2017-09-26 03:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  return string.toLowerCase() === string
-
+  string.toLowerCase() === string
+  return 'I can\'t hear you!'
 }
