8f389d9c2681a26672ca8dbc6aaffd4e3a9bac41
Jose Riano <joselriano10@gmail.com>
2017-09-26 03:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,5 @@ function sayHiToGrandma(string){
   return 'I can\'t hear you!'
 }else if(string.toUpperCase() === string)
   return "YES INDEED!"
-}else{
-return 'I love you, too.'
 }
+return 'I love you, too.'
